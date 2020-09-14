ec468a87fe0f76e67ac3fc749594e91f96076901
Ayesha Matwadia <ayeshamatwadia@gmail.com>
2020-09-14 22:57:35 +0200

    Updates

---
+++ precache-manifest.a4aafe99569536c924bad31206c3e684.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94fcc1cc59a0756a17c5cc960cccd782",
+    "url": "/ChuckNorrisJokes/index.html"
+  },
+  {
+    "revision": "9f169f25e5518a48145f",
+    "url": "/ChuckNorrisJokes/static/css/main.5afeb3f5.chunk.css"
+  },
+  {
+    "revision": "c16578f72424e81651ec",
+    "url": "/ChuckNorrisJokes/static/js/2.94661a9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ChuckNorrisJokes/static/js/2.94661a9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f169f25e5518a48145f",
+    "url": "/ChuckNorrisJokes/static/js/main.cac90232.chunk.js"
+  },
+  {
+    "revision": "f5315ffd5f35cd891ab0",
+    "url": "/ChuckNorrisJokes/static/js/runtime-main.f37253cb.js"
+  },
+  {
+    "revision": "d08beac422c319c13ef2c230283341a4",
+    "url": "/ChuckNorrisJokes/static/media/chuck_norris_image.d08beac4.jpg"
+  }
+]);
